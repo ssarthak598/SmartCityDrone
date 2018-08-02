80b4ca96237d8d360a9197f75d881a61e9254a9b
ssarthak598 <sarthak.sethi@hotmail.com>
2018-08-02 10:33:10 +0400

    Add files via upload

---
+++ VisionAPI.py
@@ -0,0 +1,32 @@
+import requests, json, os, picamera
+import urllib2
+import urllib
+
+subscription_key = "0bf8752660c0442fa6bcafc81354cc1e"
+vision_url = "https://southcentralus.api.cognitive.microsoft.com/vision/v1.0/an$
+
+camera = picamera.PiCamera()
+camera.resolution = (1920, 1080)
+camera.rotation = 90
+
+image_name = "image.jpg"
+headers    = {'Ocp-Apim-Subscription-Key': subscription_key, "Content-Type": "a$
+params     = {'visualFeatures': 'Categories,Description,Color'}
+
+while True :
+
+        raw_input("Press any key to take a picture")
+
+        camera.capture(image_name)
+        print "image taken"
+        image_data = open(image_name, "rb").read()
+        response   = requests.post(vision_url, headers=headers, params=params, $
+        analysis   = response.json()
+
+        image_caption = analysis["description"]["captions"][0]["text"].capitali$
+        tags = "<title>" + image_caption + "</title>"
+        query_args = { 'q':tags}
+        encoded_args = urllib.urlencode(query_args)
+        urlx = URi + enco$
+        resp = urllib2.urlopen(urlx).read()
+        print image_caption
